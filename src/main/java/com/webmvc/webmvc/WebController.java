e71e673ab9426121da5f63bdff283ef580746ffe
Anil Kumar <rocky.chandel77@gmail.com>
2021-08-25 13:23:37 +0530

    first commit

---
+++ src/main/java/com/webmvc/webmvc/WebController.java
@@ -0,0 +1,15 @@
+package com.webmvc.webmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WebController {
+
+	@RequestMapping(value = "/Welcome", method = RequestMethod.GET)
+	public String welcomPage(ModelMap model) {
+		return "welcome";
+	}
+}
---
+++ src/main/resources/1-data.sql
@@ -0,0 +1,17 @@
+drop table person;
+
+create table person
+(
+   id integer not null,
+   name varchar(255) not null,
+   location varchar(255),
+   birth_date timestamp,
+   primary key(id)
+);
+
+insert into person (id,name,location,birth_date) values (1001,'Anil','Mohali',curdate());
+insert into person (id,name,location,birth_date) values (1002,'Rocky','Mandi',curdate());
+insert into person (id,name,location,birth_date) values (1003,'Vijay','Bhatinda',curdate());
+insert into person (id,name,location,birth_date) values (1004,'Aman','Raipur',curdate());
+insert into person (id,name,location,birth_date) values (1005,'Ajay','NewzLand',curdate());
+insert into person (id,name,location,birth_date) values (1006,'Ankit','Shimla',curdate());
